69a60808974c60876d997c9f51061a0866b9e142
unknown <rajranjith781@gmail.com>
2019-07-24 16:40:08 +0530

    contact form

--- contact.php
+++ contact.php
@@ -6,10 +6,10 @@ $subject = $_POST['subject'];
 $mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
-$mailTo = "tech@nativevreativ.com";
+$mailTo = "RandomUser1@gmail.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-mail($mailTo, $subject, $txt);
+mail($mailTo, $subject, $txt, $headers);
 
 }
